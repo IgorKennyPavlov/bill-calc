d283721fa679aa77e75cd2d101b033ed9acb9b0d
iypavlo4 <iypavlo4@mts.ru>
2020-10-11 14:28:37 +0300

    Tesseract added; basic UI added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, ViewChild } from '@angular/core'
+import { createWorker } from 'tesseract.js'
 
 @Component({
   selector: 'app-main',
@@ -6,4 +7,42 @@ import { Component } from '@angular/core'
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  @ViewChild('img', { read: ElementRef }) imgElRef: ElementRef<HTMLImageElement>
+
+  ocrResult: string
+  imgSrc = ''
+
+  async onLoad(e: Event) {
+    const input = (e as InputEvent).target as HTMLInputElement
+    const files = input.files
+    const file = files[0]
+
+    try {
+      this.imgSrc = await this._toBase64(file)
+    } catch (err) {
+      console.log(err)
+    }
+
+    this._handleImgs(files)
+  }
+
+  private async _handleImgs(files: FileList) {
+    const worker = createWorker({
+      // logger: m => console.log(m)
+    })
+    await worker.load()
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
+    await worker.setParameters({ tessedit_char_whitelist: '0123456789.,' })
+    const { data: { text } } = await worker.recognize(files[0])
+    this.ocrResult = text
+    await worker.terminate()
+  }
+
+  private _toBase64 = (file: File): Promise<string> => new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = error => reject(error)
+  })
 }
